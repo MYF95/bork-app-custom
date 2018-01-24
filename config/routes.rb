52a8eb073f485dce0cc21fa10055b460e0bdcdc3
Mack Yao Fu <mack@secret-source.eu>
2018-01-24 13:17:52 +0000

    Finish user setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/chicken', to: 'static_pages#chicken'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
