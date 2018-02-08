2e2bebb41a56d08029aa3df88b49dd9b4db0ebd8
Mack Yao Fu <mack@secret-source.eu>
2018-02-08 18:11:57 +0000

    microposts functionality, still missing adding images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@ User.create!(name: "Mikota", email: "myf1995@gmail.com", password: "chicken", pa
 User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", activated: true, activated_at: Time.zone.now)
 
 95.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  name = Faker::LeagueOfLegends.champion
+  email = "example-#{n+1}@bork.com"
   password = "password"
   User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+
+50.times do
+  content = Faker::LeagueOfLegends.quote
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
